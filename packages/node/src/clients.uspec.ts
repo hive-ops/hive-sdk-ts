e0ae0ba7d7a687a062da13c61f19bf6614c9f859
Adeniyi Bello <poph2@users.noreply.github.com>
2025-04-15 22:49:27 -0400

    chore: update @hiveops/node to version 0.0.14; update @hiveops/core import and enhance database record insertion tests

--- packages/node/src/clients.uspec.ts
+++ packages/node/src/clients.uspec.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { getStackHRN, initialize, RecordItem, Records } from "@hiveops/core";
+import { getStackHRN, initialize, Record, RecordItem, Records } from "@hiveops/core";
 import dotenv from "dotenv";
 import { createSingletonBeekeeperClient, createSingletonTokenClient, createSingletonVespaClient } from "./clients";
 
@@ -42,6 +42,30 @@ describe("Hive SDK Clients", () => {
       records: new Records({
         columnNames: ["firstName", "lastName", "email", "age"],
         items: [new RecordItem({ values: [faker.person.firstName(), faker.person.lastName(), faker.internet.email(), "30"] })],
+      }),
+    });
+    expect(res2).toBeDefined();
+    expect(res2.insertedIds).toHaveLength(1);
+    expect(res2.insertedIds[0]).toBeDefined();
+    expect(res2.insertedIds[0]).not.toBe("");
+  });
+
+  it("should insert one record into the database", async () => {
+    const res = await beekeeperClient.getVespaDatabaseStack({
+      hrn: getStackHRN(),
+    });
+    const stack = res.stack!;
+    expect(stack).toBeDefined();
+    expect(stack.databases).toHaveLength(1);
+    const database = stack.databases[0];
+
+    const vespaClient = createSingletonVespaClient(database);
+    const res2 = await vespaClient.insertRecord({
+      databaseHrn: database.hrn,
+      tableName: "User",
+      record: new Record({
+        columnNames: ["firstName", "lastName", "email", "age"],
+        item: new RecordItem({ values: [faker.person.firstName(), faker.person.lastName(), faker.internet.email(), "30"] }),
       }),
     });
     expect(res2).toBeDefined();
