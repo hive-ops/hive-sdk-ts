3e5de9bfc6b48e01c1a774bdd01a5cd3c731b52b
Adeniyi Bello <poph2@users.noreply.github.com>
2025-08-14 17:42:27 -0400

    Update initialize.ts

--- packages/web/src/initialize.ts
+++ packages/web/src/initialize.ts
@@ -1,7 +1,7 @@
 import { FirebaseToken, createTokenManager, setClientOptions } from "@hiveops/core";
 import { clientOptions } from "./client-options";
 
-export const  = (getTokenFunc: () => Promise<FirebaseToken>) => {
+export const initializeClient = (getTokenFunc: () => Promise<FirebaseToken>) => {
   setClientOptions(clientOptions);
   createTokenManager(getTokenFunc, true);
 };
